3fc5e49638dc51a8033b79478ab685b316df007f
David Hübscher <HuebschersDavid@gmail.com>
2018-01-27 12:59:33 +0100

    Initial commit: Simple Version of UrlDeToUs with hard coded strings for replacing '/de-de/', '/de_de/' or '/de/' with the english versions.

---
+++ changeUrl.js
@@ -0,0 +1,42 @@
+function initializePageAction(tab) {
+  if (tab.url.toLowerCase().includes("/de-de/") || tab.url.toLowerCase().includes("/de/") || tab.url.toLowerCase().includes("/de_de/")) {
+    browser.pageAction.show(tab.id);
+  }
+  else {
+    browser.pageAction.hide(tab.id);
+  }
+}
+
+var gettingAllTabs = browser.tabs.query({});
+gettingAllTabs.then((tabs) => {
+  for (let tab of tabs) {
+    initializePageAction(tab);
+  }
+});
+
+browser.tabs.onUpdated.addListener((id, changeInfo, tab) => {
+  initializePageAction(tab);
+});
+
+function onUpdated(tab) {
+  initializePageAction(tab);
+}
+
+function onError(error) {}
+
+function changeUrl(tabInfo) {
+  if (tabInfo != null) {
+    var url = tabInfo.url.replace(/\/de-de\//i, "/en-us/");
+    url = url.replace(/\/de\//i, "/en/");
+    url = url.replace(/\/de_de\//i, "/en_us/");
+    if (url != tabInfo.url) {
+      var updating = browser.tabs.update( 
+        tabInfo.id,
+        { url: url }
+      );
+      updating.then(onUpdated, onError);
+    }
+  }
+}
+
+browser.pageAction.onClicked.addListener(changeUrl);
